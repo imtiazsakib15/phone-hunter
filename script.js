c59df2f669c00037135a6b017a9ef75437871988
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-08-28 12:30:40 +0600

    products and search button added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const loadPhone = async () => {
+const loadPhone = async (brand) => {
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${brand}`
   );
   const data = await response.json();
   const phones = data.data;
@@ -10,31 +10,35 @@ const loadPhone = async () => {
 const cardContainer = document.getElementById("card-container");
 
 const displayPhones = (phones) => {
+  cardContainer.innerHTML = "";
   phones.forEach((phone) => {
     const cardDiv = document.createElement("div");
-    const {} = phone;
-    console.log(phone);
+    cardDiv.classList.add(
+      "w-max",
+      "bg-white",
+      "border",
+      "border-gray-200",
+      "rounded-lg",
+      "shadow",
+      "dark:bg-gray-800",
+      "dark:border-gray-700"
+    );
+    const { brand, phone_name, slug, image } = phone;
 
     const phoneCard = `
-    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-      <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
-    </a>
-    <div class="px-5 pb-5">
-      <a href="#">
-        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-        </h5>
-      </a>
+        <img class="p-8 mx-auto rounded-t-lg" src='${image}' alt="product image" />
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            ${phone_name}
+          </h5>
+        </a>
 
-      <div class="flex items-center justify-between">
-        <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-        <a href="#"
-          class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700">Add
-          to cart</a>
+          <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+          <a href="#"
+            class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700">Add
+            to cart</a>
       </div>
-    </div>
-  </div>
     `;
 
     cardDiv.innerHTML = phoneCard;
@@ -42,4 +46,12 @@ const displayPhones = (phones) => {
   });
 };
 
-loadPhone();
+const handleSearch = () => {
+  const searchField = document.getElementById("search-field");
+  // console.log(searchField);
+  const brand = searchField.value;
+  console.log(brand);
+  loadPhone(brand);
+};
+
+// loadPhone("samsung");
